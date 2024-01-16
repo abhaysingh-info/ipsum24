c690bcf2d053e86f22a45a2494441ff8e75e9ce9
Abhay Singh <abhay1029singh@gmail.com>
2024-01-16 12:17:00 +0530

    added images

--- src/routes/events/EventsCarousel.svelte
+++ src/routes/events/EventsCarousel.svelte
@@ -1,3 +1,72 @@
+<script>
+	let games = [
+		[
+			{
+				name: 'Business Blueprint Bash',
+				poster: '/events/Business Blueprint Bash.jpg',
+				posterLowRes: '/events/Business Blueprint Bash-lowres.jpg',
+				date: '20th March 2021'
+			},
+			{
+				name: 'Clash of Minds: Business Edition',
+				poster: '/events/Clash of Minds: Business Edition.jpg',
+				posterLowRes: '/events/Clash of Minds: Business Edition-lowres.jpg',
+				date: '20th March 2021'
+			},
+			{
+				name: 'Trade Trivia Tournament - Paper Trading',
+				poster: '/events/Trade Trivia Tournament - Paper Trading.jpg',
+				posterLowRes: '/events/Trade Trivia Tournament - Paper Trading-lowres.jpg',
+				date: '20th March 2021'
+			},
+			{
+				name: 'Market Mingle',
+				poster: '/events/Market Mingle.jpg',
+				posterLowRes: '/events/Market Mingle-lowres.jpg',
+				date: '20th March 2021'
+			},
+			{
+				name: 'BizQuiz Bonanza',
+				poster: '/events/BizQuiz Bonanza.jpg',
+				posterLowRes: '/events/BizQuiz Bonanza-lowres.jpg',
+				date: '20th March 2021'
+			}
+		],
+		[
+			{
+				name: '1-min Speech',
+				poster: 'images/event/event1.jpg',
+				posterLowRes: 'images/event/event1.jpg',
+				date: '20th March 2021'
+			},
+			{
+				name: '1-Min Games',
+				poster: 'images/event/event1.jpg',
+				posterLowRes: 'images/event/event1.jpg',
+				date: '20th March 2021'
+			},
+			{
+				name: 'Biz Hunt',
+				posterLowRes: 'images/event/event1.jpg',
+				poster: 'images/event/event1.jpg',
+				date: '20th March 2021'
+			},
+			{
+				name: 'PublicityPulse: The PR Event',
+				poster: 'images/event/event1.jpg',
+				posterLowRes: 'images/event/event1.jpg',
+				date: '20th March 2021'
+			},
+			{
+				name: 'Design Dual(logo/ tagline/ brand ad)',
+				poster: 'images/event/event1.jpg',
+				posterLowRes: 'images/event/event1.jpg',
+				date: '20th March 2021'
+			}
+		]
+	];
+</script>
+
 <section id="event">
 	<div class="container">
 		<div class="row">
@@ -11,56 +80,21 @@
 						><i class="fa fa-angle-right"></i></a
 					>
 					<div class="carousel-inner">
-						<div class="item active">
-							<div class="row">
-								<div class="col-sm-4">
-									<div class="single-event">
-										<img class="img-responsive" src="images/event/event1.jpg" alt="event-image" />
-										<h4>Chester Bennington</h4>
-										<h5>Vocal</h5>
-									</div>
-								</div>
-								<div class="col-sm-4">
-									<div class="single-event">
-										<img class="img-responsive" src="images/event/event2.jpg" alt="event-image" />
-										<h4>Mike Shinoda</h4>
-										<h5>vocals, rhythm guitar</h5>
-									</div>
-								</div>
-								<div class="col-sm-4">
-									<div class="single-event">
-										<img class="img-responsive" src="images/event/event3.jpg" alt="event-image" />
-										<h4>Rob Bourdon</h4>
-										<h5>drums</h5>
-									</div>
+						{#each games as game, i}
+							<div class="item {i === 0 ? 'active' : ''}">
+								<div class="row">
+									{#each game as event}
+										<div class="col-sm-4">
+											<div class="single-event">
+												<img class="img-responsive" src={event.poster} alt={event.name} />
+												<h4>{event.name}</h4>
+												<p>{event.date}</p>
+											</div>
+										</div>
+									{/each}
 								</div>
 							</div>
-						</div>
-						<div class="item">
-							<div class="row">
-								<div class="col-sm-4">
-									<div class="single-event">
-										<img class="img-responsive" src="images/event/event1.jpg" alt="event-image" />
-										<h4>Chester Bennington</h4>
-										<h5>Vocal</h5>
-									</div>
-								</div>
-								<div class="col-sm-4">
-									<div class="single-event">
-										<img class="img-responsive" src="images/event/event2.jpg" alt="event-image" />
-										<h4>Mike Shinoda</h4>
-										<h5>vocals, rhythm guitar</h5>
-									</div>
-								</div>
-								<div class="col-sm-4">
-									<div class="single-event">
-										<img class="img-responsive" src="images/event/event3.jpg" alt="event-image" />
-										<h4>Rob Bourdon</h4>
-										<h5>drums</h5>
-									</div>
-								</div>
-							</div>
-						</div>
+						{/each}
 					</div>
 				</div>
 			</div>
