4d7a9c02c2cee9af352b4c00ee54bc01cdc02591
Abhay Singh <abhay1029singh@gmail.com>
2024-01-19 11:32:44 +0530

    done

--- src/routes/About.svelte
+++ src/routes/About.svelte
@@ -5,12 +5,25 @@
 	<div class="about-content">
 		<h2>About Event</h2>
 		<p>
-			IPSUM 2K24 is a inter-college level event being organised by the Department of BBA and
-			BBA(IB), MIT Arts, Commerce and Science College, Alandi, Pune. The seminar is being organised
-			with the aim of providing a platform for researchers, academicians and industry professionals
-			to share their knowledge and research work in the field of computer science and engineering.
-			The seminar will be held on 03-03 Feburary 2024.
+			Mark your calendars for February 2nd-3rd as the campus in Alandi, Pune, transforms into a
+			playground for budding titans and artistic rebels. IPSUM 2K24 promises two days of fierce
+			competition, where you'll craft winning business models, conquer virtual markets in paper
+			trading, and dominate PR battles. But amidst the intellectual gladiatorial arena, unleash your
+			inner child with Squid Game thrills, UNO showdowns, and the hilarious "Paint Me Blind"
+			challenge. Can you crack the cryptic clues of "Guess the Hasti"?
 		</p>
+		<p>
+			Beyond trophies and certificates, IPSUM 2K24 ignites your potential. Fire up your voice in
+			fiery debates and extempore speeches, learn from industry pros in insightful workshops, and
+			forge a network of kindred spirits. This is your chance to sharpen your business acumen,
+			ignite your creativity, and make memories that'll echo long after the fest ends.
+		</p>
+		<p>
+			So, are you ready to unleash your inner maverick? Register now and join the IPSUM 2K24
+			revolution! Let your business brilliance and creative genius take center stage. Win big, have
+			fun, and become a legend.
+		</p>
+		<p>Don't miss out!</p>
 		<a href="#explore" class="btn btn-primary">View Events <i class="fa fa-angle-right"></i></a>
 	</div>
 </section>
--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,17 +1,7 @@
-<script>
-	import Contact from './Contact.svelte';
-
-	import Sponsors from './Sponsors.svelte';
-
-	import About from './About.svelte';
-
-	import Navbar from './Navbar.svelte';
-</script>
-
 <footer id="footer">
 	<div class="container">
 		<div class="text-center">
-			<p>Copyright &copy;2024<a> IPSUN 2K24 </a></p>
+			<p>Copyright &copy;2024<a> IPSUM 2K24 </a></p>
 		</div>
 	</div>
 </footer>
